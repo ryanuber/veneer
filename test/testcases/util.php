5f74b718e129903d4318a3f2bbb1452e35276fdd
Ryan Uber <ru@ryanuber.com>
2013-02-28 22:57:43 -0800

    Fixing a few errors in util.php test cases

--- test/testcases/util.php
+++ test/testcases/util.php
@@ -5,14 +5,14 @@ class test_util extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals(
             'path/to/some/file',
-            \veneer\util::path_join(array('path', 'to', 'some', 'file'));
-        }
+            \veneer\util::path_join(array('path', 'to', 'some', 'file'))
+        );
     }
 
     public function test_version_munger()
     {
-        $this->assertEquals('v5.9', \veneer\util::version('number', 'v5_9'))
-        $this->assertEquals('v5_9', \veneer\util::version('class', 'v5.9'))
+        $this->assertEquals('v5.9', \veneer\util::version('number', 'v5_9'));
+        $this->assertEquals('v5_9', \veneer\util::version('class', 'v5.9'));
         $this->assertFalse(\veneer\util::version('oogabooga', 'v5.9'));
     }
 }
