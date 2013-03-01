8033f200447824512f1c7721d3b698ff8fdb2fc4
Ryan Uber <ru@ryanuber.com>
2013-02-28 21:58:04 -0800

    Updating Travis CI configuration and app.php tests

--- test/testcases/app.php
+++ test/testcases/app.php
@@ -4,7 +4,7 @@ class test_veneer_app extends PHPUnit_Framework_TestCase
     public function test_defaults_set_and_get()
     {
         $this->assertTrue(\veneer\app::set_default('output_handler', 'some_handler'));
-        $this->assertEquals(\veneer\app::get_default('output_handler'), 'some_handler');
+        $this->assertEquals('some_handler', \veneer\app::get_default('output_handler'));
     }
 
     public function test_run()
