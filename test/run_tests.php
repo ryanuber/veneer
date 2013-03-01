b1318162477b0bf2217bf60831a84009bd95c98b
Ryan Uber <ru@ryanuber.com>
2013-02-28 22:55:54 -0800

    Adding basic util.php test cases

--- test/run_tests.php
+++ test/run_tests.php
@@ -4,3 +4,4 @@ include 'testcases/output_handler_serialize.php';
 include 'testcases/output_handler_plain.php';
 include 'testcases/output_handler_html.php';
 include 'testcases/app.php';
+include 'testcases/util.php';
